fb6b7604298538592daf3437ca000169b765b7b1
shreyaa2103 <59549971+shreyaa2103@users.noreply.github.com>
2023-04-15 21:37:09 +0530

    Add files via upload

---
+++ A3_v10.cpp
@@ -0,0 +1,238 @@
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <memory>
+#include <vector>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+#define INITIALRUNSIZE 809714 // 104857 // 104857
+// #define MAXSIZE 15728640 // 800ULL * 1024ULL * 1024ULL
+const uint64_t maxsize = 80ULL * 1024ULL * 1024ULL; // 800MB
+
+template <typename T>
+class List
+{
+private:
+    std::vector<T> data;
+public:
+    List() {}
+    ~List() {}
+    void push_back(const T &value)
+    {
+        data.push_back(value);
+    }
+    void sort()
+    {
+        std::sort(data.begin(), data.end());
+    }
+    int size() const
+    {
+        return data.size();
+    }
+    T &front()
+    {
+        return data.front();
+    }
+    void pop_front()
+    {
+        data.erase(data.begin());
+    }
+    typename std::vector<T>::iterator begin()
+    {
+        return data.begin();
+    }
+    typename std::vector<T>::iterator end()
+    {
+        return data.end();
+    }
+    typename std::vector<T>::const_iterator begin() const
+    {
+        return data.begin();
+    }
+
+    typename std::vector<T>::const_iterator end() const
+    {
+        return data.end();
+    }
+};
+
+List<string> runs;
+
+struct Node
+{
+public:
+    string data;
+    int line;
+    Node(string a, int b)
+    {
+        data = a;
+        line = b;
+    }
+};
+
+struct compareVal
+{
+    bool operator()(Node *p1, Node *p2)
+    {
+        return p1->data > (p2->data); //*ascending order sort
+    }
+};
+
+void mergeRuns(const List<string> &inputFiles, const string &outputFile)
+{
+    // Open all the input files in read mode
+    vector<ifstream> input_file_ptrs;
+    for (auto const &file : inputFiles)
+    {
+        input_file_ptrs.emplace_back(move(file));
+    }
+    // Open the output file in write mode
+    ofstream output_file_ptr(outputFile);
+    // Initialize a priority queue to keep track of the smallest line from each file
+    priority_queue<Node *, vector<Node *>, compareVal> pq;
+    // Read the first line from each file and store them in the priority queue
+    string line;
+    for (int i = 0; i < input_file_ptrs.size() && getline(input_file_ptrs[i], line); i++)
+    {
+        pq.emplace(new Node(line, i));
+    }
+    // Merge the files
+    while (!pq.empty())
+    {
+        auto min_line = pq.top();
+        pq.pop();
+        output_file_ptr << min_line->data << "\n";
+        string line;
+        if (getline(input_file_ptrs[min_line->line], line))
+        {
+            pq.emplace(new Node(line, min_line->line));
+        }
+    }
+    // Close all the input files and the output file
+    for (auto &input_file_ptr : input_file_ptrs)
+    {
+        input_file_ptr.close();
+    }
+    output_file_ptr.close();
+}
+
+void createInitialRuns(const string &inputF, const long key_count)
+{
+    ifstream inputFile(inputF);
+    int runNum = 0;
+    int keys = 0;
+    string line;
+    uint64_t bytes_written = 0;
+    while (getline(inputFile, line) && keys < key_count)
+    {
+        string fileName = "temp.0." + to_string(runNum++);
+        ofstream outputFile(fileName);
+        runs.push_back(fileName);
+        deque<string> temp;
+        temp.push_back(move(line));
+        bytes_written += static_cast<uint64_t>(line.size() + 1);
+        // bytes_written += line.size() + 1;
+        // cout << bytes_written << " ";
+        // for (int ct = 0; ct < INITIALRUNSIZE && getline(inputFile, line) && keys < key_count; ct++)
+        while (bytes_written < maxsize && getline(inputFile, line) && keys < key_count)
+        {
+            temp.push_back(move(line)); // use std::move to avoid copying
+            bytes_written += static_cast<uint64_t>(line.size() + 1);
+            // bytes_written += line.size() + 1;
+            // cout << bytes_written << " ";
+            keys++;
+        }
+        // introsort(temp);
+        sort(temp.begin(), temp.end());
+        int t = temp.size();
+        while (t-- != 0)
+        {
+            outputFile << temp.front() << "\n";
+            // bytes_written += temp.front().size() + 1;
+            temp.pop_front(); 
+        }
+        bytes_written = 0;
+    }
+}
+
+int external_merge_sort_withstop(const char *input, const char *output,
+                                 const long key_count, const int k = 2, const int num_merges = 0)
+{
+    try
+    {
+        createInitialRuns(input, key_count);
+        int numMerges = num_merges;
+        if (numMerges == 0)
+        {
+            numMerges = INT16_MAX;
+        }
+
+        int t = runs.size(), totalMergesSoFar = 0;
+        int stage = 1;
+
+        if (runs.size() == 1) // only one run, directly output to output file
+        {
+            ifstream inFile(runs.front());
+            ofstream outFile(output);
+            outFile << inFile.rdbuf();
+            inFile.close();
+            outFile.close();
+            return 0;
+        }
+
+        while (runs.size() != 1)
+        {
+            int totalRuns = runs.size();
+            int runNum = 0;
+            int start = 0, end = k - 1;
+            while (start < totalRuns)
+            {
+                // list<string> newVec;
+                List<string> newVec;
+                string outFile = totalRuns <= k ? output : "temp." + to_string(stage) + "." + to_string(runNum++);
+
+                for (int i = start; i <= min(end, totalRuns - 1); i++)
+                {
+                    newVec.push_back(runs.front());
+                    runs.pop_front();
+                }
+
+                mergeRuns(newVec, outFile);
+                totalMergesSoFar++;
+                runs.push_back(outFile);
+                start = end + 1;
+                end = end + k;
+                if (totalMergesSoFar > numMerges)
+                    return totalMergesSoFar;
+            }
+            stage++;
+        }
+        return totalMergesSoFar;
+    }
+    catch (const std::exception &e)
+    {
+        return -1;
+    }
+}
+
+int main()
+{
+    auto begin = std::chrono::high_resolution_clock::now();
+    // int totalMerges = external_merge_sort_withstop("random_10gb.list", "output", 2526350, 8);
+    int totalMerges = external_merge_sort_withstop("random.list", "output", 2530475, 8);
+    // int totalMerges = external_merge_sort_withstop("englishsubset.txt", "output", 1000000);
+    // int totalMerges = external_merge_sort_withstop("test3", "output", 100000000, 16);
+    // int totalMerges = external_merge_sort_withstop("random3.list", "output", 100000000, 16);
+    // int totalMerges = external_merge_sort_withstop("random3.list", "output", 5060950, 16);
+    // int totalMerges = external_merge_sort_withstop("random3.list", "output", 50609500, 16);
+    // int totalMerges = external_merge_sort_withstop("random.txt", "output", 100000000, 16);
+    cout << totalMerges;
+    auto end = std::chrono::high_resolution_clock::now();
+    auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - begin);
+    cout << "\nTime measured: " << elapsed.count() * 1e-9 << " seconds.\n";
+}
